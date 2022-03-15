d36f41280e03d905577e7ef22441854ea186ae3d
LauraOrte <95409913+LauraOrte@users.noreply.github.com>
2022-03-15 17:25:06 +0100

    Delete functions.js

--- Middleware/functions.js
+++
@@ -1,9 +0,0 @@
-const dataJson = require('./parameters.json')
-
-const calculator = {
-    suma: (a, b) => a + b,
-    resta: (a, b) => a - b,
-    multiplicacio: (a, b) => a * b,
-  };
-  
-  module.exports = calculator;
