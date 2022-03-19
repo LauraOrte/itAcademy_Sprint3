b318ba89f8651040831a111499d4719ba24b8daf
LauraOrte <laura.orte7@gmail.com>
2022-03-19 19:02:45 +0100

    Decorator corregido

--- Decorator/Decorator.js
+++ Decorator/Decorator.js
@@ -1,33 +1,23 @@
 const div = require('./currency_conversions.json');
 
 
-class Decorator {
-    constructor(producto) {
-        this.producto = producto;
-    };
-
-    elProducto() {
-        return this.producto;
-    };
-
-    convertidor(divisa) {
+function convertidor(Producto, divisa) {
         try {
-            const regex = new RegExp(this.elProducto().divisa + '_' + divisa);
-            const conversion = this.elProducto().precio * Object.entries(div).find(n => regex.test(n))[1]
+            const regex = new RegExp(Producto.divisa + '_' + divisa);
+            const conversion = Producto.precio * Object.entries(div).find(n => regex.test(n))[1]
 
-            console.log(`Se hará la conversión de: ${this.elProducto().divisa} a ${divisa}`);
+            console.log(`Se hará la conversión de: ${Producto.divisa} a ${divisa}`);
 
-            console.log(`${this.elProducto().nom} ${this.elProducto().modelo} con un precio de: ${this.elProducto().precio} ${this.elProducto().divisa} será de: ${conversion.toFixed(2)} ${divisa} en el cambio de divisa`);
+            console.log(`${Producto.nom} ${Producto.modelo} con un precio de: ${Producto.precio} ${Producto.divisa} será de: ${conversion.toFixed(2)} ${divisa} en el cambio de divisa`);
         } catch (error) {
             if(divisa !== 'EUR'){
                 console.log('Solo se puede hacer la conversión a Euros');
-            }else if(this.elProducto().divisa === 'EUR'){
+            }else if(Producto.divisa === 'EUR'){
                 console.log('Solo se hará la conversión a euros, cambie la divisia inicial')
             }else{
                 console.log(`La divisia no existe`);
             }
         }
     };
-}
 
-module.exports = Decorator;
+module.exports = convertidor;
--- Decorator/app.js
+++ Decorator/app.js
@@ -1,17 +1,12 @@
 const Producto = require('./producto');
-const Decorator = require('./Decorator');
+const convertidor = require('./Decorator');
 
 
 const nintendo = new Producto('nintendo', '64', 150, 'USD');
 const ferrari = new Producto('ferrari', 'i40', 50000, 'GBP');
 const lg = new Producto('lg', 'p20', 500, 'CAD');
 
-
-const nintendo2 = new Decorator(nintendo);
-const ferrari2 = new Decorator(ferrari);
-const lg2 = new Decorator(lg);
-
-nintendo2.convertidor('EUR');
-ferrari2.convertidor('EUR');
-lg2.convertidor('EUR');
+convertidor(nintendo, 'EUR');
+convertidor(ferrari, 'EUR');
+convertidor(lg, 'EUR');
 
